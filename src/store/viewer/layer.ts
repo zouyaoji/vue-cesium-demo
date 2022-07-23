fd172d2b685df16ed444b1fa5ac6c6a4d01f96c1
zouyaoji <370681295@qq.com>
2022-07-23 11:14:04 +0800

    fix: 修复线上环境静态资源访问不了的问题

--- src/store/viewer/layer.ts
+++ src/store/viewer/layer.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zouyaoji@https://github.com/zouyaoji
  * @Date: 2021-09-15 11:06:15
- * @LastEditTime: 2022-07-20 14:00:47
+ * @LastEditTime: 2022-07-23 11:09:11
  * @LastEditors: zouyaoji
  * @Description:
  * @FilePath: \vue-cesium-demo\src\store\viewer\layer.ts
@@ -292,7 +292,7 @@ export const useLayerStore = defineStore('layer', {
           stroke: '#ffc107',
           fill: 'transparent',
           show: false,
-          data: '/datas/china.json'
+          data: './datas/china.json'
         }
       },
       {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zouyaoji@https://github.com/zouyaoji
  * @Date: 2021-12-08 23:26:13
- * @LastEditTime: 2022-07-23 00:55:19
+ * @LastEditTime: 2022-07-23 11:08:51
  * @LastEditors: zouyaoji
  * @Description:
  * @FilePath: \vue-cesium-demo\vite.config.ts
@@ -26,7 +26,7 @@ const htmlPlugin = () => {
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   return defineConfig({
-    base: process.env.VITE_VUE_ROUTER_BASE,
+    base: './',
     plugins: [
       vue({
         template: { transformAssetUrls }
